56cacc0316c6f35038ee55a52af7eebc31af1637
Ildevdio <diogoswc@gmail.com>
2026-07-20 07:22:34 -0300

    Simplify start.ps1: use --force to bypass incremental build issues

--- Multigrao.Api/start.ps1
+++ Multigrao.Api/start.ps1
@@ -1,4 +1,2 @@
-if (Test-Path obj) { Remove-Item -Recurse -Force obj -ErrorAction SilentlyContinue; Start-Sleep -Milliseconds 500 }
-if (Test-Path bin) { Remove-Item -Recurse -Force bin -ErrorAction SilentlyContinue; Start-Sleep -Milliseconds 500 }
-New-Item -ItemType Directory -Force -Path obj\Debug\net10.0\staticwebassets -ErrorAction SilentlyContinue | Out-Null
-dotnet build -q && dotnet bin\Debug\net10.0\Multigrao.Api.dll --urls "http://localhost:5050"
+dotnet build --force 2>&1
+if ($LASTEXITCODE -eq 0) { dotnet bin\Debug\net10.0\Multigrao.Api.dll --urls "http://localhost:5050" }
